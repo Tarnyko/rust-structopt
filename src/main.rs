1b974ddd1351ce2c260b72fa4b6763dfca5adf52
Tarnyko <Tarnyko tarnyko@tarnyko.net>
2021-10-27 16:06:55 +0200

    Add CleverCloud CI actions.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use structopt::StructOpt;
+use std::net::TcpListener;
+use std::io::Write;    // for TcpStream.flush()
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "example", about = "An example of StructOpt usage.")]
@@ -8,5 +10,15 @@ struct Opt {
 
 fn main() {
     let opt = Opt::from_args();
-    println!("Hello, structopt: {:?}", opt);
+    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();    // "mut" for .flush()
+        let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+
+        println!("Hello, structopt: {:?}", opt);
+    }
 }
